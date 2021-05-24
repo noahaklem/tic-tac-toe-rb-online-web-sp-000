2eafd06362f304fe80635b4cc4580985cc6520d1
industrious-agent-153d <klem.noah@gmail.com>
2021-05-24 03:24:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,7 +102,11 @@ def play(board)
   until over?(board) == true
     turn(board)
   end
-  
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Woah that was a close one! Cats game!"
+  end
 end
 
 
